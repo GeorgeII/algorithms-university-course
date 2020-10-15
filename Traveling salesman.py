f53159eb2d55543a4e16ab543fb88f85d96a31fc
GeorgeII <egorzorikov@gmail.com>
2020-10-15 15:58:31 +0400

    Ant colony optimization update

--- Traveling salesman.py
+++ Traveling salesman.py
@@ -85,25 +85,83 @@ def bruteforce_helper(graph, start_and_end_node, current_node, path, path_length
     return
 
 
-def ant_colony(graph: nx.Graph, start_and_end_node, number_of_ants=3, alpha=0.5, beta=1.1, pheromone_evaporate_coefficient=0.5):
+def ant_colony(graph: nx.Graph, start_and_end_node, number_of_ants=3, alpha=1, beta=1,
+               pheromone_evaporation_coefficient=0.2, pheromone_by_ant_coefficient=1):
+    """
+    One critical restriction is required for this algorithm: a graph must be complete.
+    Every ant finds a Hamiltonian path leaving pheromone on an edge after every iteration.
+    """
+
     # copy the original graph so we can add some info to nodes and edges.
     graph = graph.copy()
 
+    # initialize pheromone for all edges
+    nx.set_edge_attributes(graph, 1, 'pheromone')
+
+    # create ants
     ants_current_nodes = np.random.choice(graph.number_of_nodes(), number_of_ants, replace=False)
     print(ants_current_nodes)
 
-    neighbors_attractiveness = graph
+    # list of sets. Each set controls nodes that are left for a particular ant
+    ants_nodes_left = []
+    for ant_node in ants_current_nodes:
+        nodes_left = set(graph.nodes)
+        nodes_left.remove(ant_node)
+        ants_nodes_left.append(nodes_left)
 
-    # Attractiveness of the next node based on the pheromone.
-    tau = 1
+    probability_table_for_every_ant = []
+    ants_next_nodes = []
 
-    # Heuristic for state transition. Usually, it's 1/d, where d is the Euclidean distance to the next node.
-    # But as we have no Euclidean distance determined in this task we just put it to be 1.
-    eta = 1
+    for ant in ants_current_nodes:
+        ant_neighbors = [n for n in graph.neighbors(ant)]
+        aggregated_pheromone_for_all_neighbors = np.array([graph.get_edge_data(ant, next_node)['pheromone'] for next_node in ant_neighbors])
+        probability_for_neighbors = []
 
-    numerator = tau**alpha + eta**beta
-    denominator = np.sum(1)
-    probability_to_move = numerator / denominator
+        for neighbor in ant_neighbors:
+            # Heuristic for state transition. Usually, it's 1/d, where d is the Euclidean distance to the next node.
+            # But as we have no Euclidean distance determined in this task we just put it to be 1.
+            eta = 1
+
+            # Attractiveness of the next node based on the pheromone.
+            tau = graph.get_edge_data(ant, neighbor)['pheromone']
+
+            numerator = tau ** alpha * eta ** beta
+            denominator = np.sum(aggregated_pheromone_for_all_neighbors**alpha * tau**beta)
+            probability_to_move_to_this_neighbor = numerator / denominator
+
+            probability_for_neighbors.append(probability_to_move_to_this_neighbor)
+
+        # Store all important information in the following format: (current node (ant), neighbors : their probabilities).
+        # It must be done to guarantee that it works even if graph.neighbors(ant) returns an unordered list.
+        info_of_ant = (ant, dict(zip(ant_neighbors, probability_for_neighbors)))
+        probability_table_for_every_ant.append(info_of_ant)
+        print(info_of_ant)
+
+        next_node = np.random.choice(ant_neighbors, 1, p=probability_for_neighbors)[0]
+        print(next_node)
+        ants_next_nodes.append(next_node)
+
+
+    # Global update of pheromones.
+
+    # evaporation on all edges
+    print(graph.edges.data('pheromone'))
+    for i in range(len(graph.nodes)):
+        for j in range(i + 1, len(graph.nodes)):
+            graph.get_edge_data(i, j)['pheromone'] *= 1 - pheromone_evaporation_coefficient
+    print(graph.edges.data('pheromone'))
+
+    # add pheromone to an edge which was chosen by an ant
+    for i in range(len(ants_current_nodes)):
+        edge = graph.get_edge_data(ants_current_nodes[i], ants_next_nodes[i])
+        edge['pheromone'] += pheromone_by_ant_coefficient / edge['weight']
+    print(graph.edges.data('pheromone'))
+
+
+
+def ant_colony_helper(graph: nx.Graph, start_and_end_node, number_of_ants, alpha, beta,
+               pheromone_evaporation_coefficient, pheromone_by_ant_coefficient):
+    pass
 
 
 if __name__ == "__main__":
