ac8b247c3bacdf87fa4cabd4bbf7b084445566c7
GeorgeII <egorzorikov@gmail.com>
2020-11-29 17:16:45 +0400

    Simple game added

--- 1 task from the book (polynomial roots)/gp.py
+++ 1 task from the book (polynomial roots)/gp.py
@@ -205,5 +205,124 @@ def getrankfunction(dataset):
         return scores
     return rankfunction
 
-rf=getrankfunction(buildhiddenset())
-evolve(2,500,rf,mutationrate=0.2,breedingrate=0.1,pexp=0.7,pnew=0.1)
+#rf=getrankfunction(buildhiddenset())
+#evolve(2,500,rf,mutationrate=0.2,breedingrate=0.1,pexp=0.7,pnew=0.1)
+
+
+
+
+
+
+
+
+# Simple game begins here.
+
+def gridgame(p):
+    # Размер доски
+    max=(3,3)
+
+    # Запоминаем последний ход каждого игрока
+    lastmove=[-1,-1]
+
+    # Запоминаем положения игроков
+    location = [[randint(0,max[0]),randint(0,max[1])]]
+
+    # Располагаем второго игрока на достаточном удалении от первого
+    location.append([(location[0][0]+2)%4,(location[0][1]+2)%4])
+
+    # Не более 50 ходов до объявления ничьей
+    for o in range(50):
+        # Для каждого игрока
+        for i in range(2):
+            locs = location[i][:] + location[1 - i][:]
+            locs.append(lastmove[i])
+            move = p[i].evaluate(locs) % 4
+
+            # Если игрок два раза подряд ходит в одном направлении, ему
+            # засчитывается проигрыш
+            if lastmove[i]==move:
+                return 1-i
+            lastmove[i]=move
+            if move==0:
+                location[i][0] -= 1
+                # Доска ограничена
+                if location[i][0]<0: location[i][0]=0
+            if move == 1:
+                location[i][0] += 1
+                if location[i][0] > max[0]: location[i][0] = max[0]
+            if move == 2:
+                location[i][1] -= 1
+                if location[i][1] < 0: location[i][1] = 0
+            if move == 3:
+                location[i][1] += 1
+                if location[i][1] > max[1]: location[i][1] = max[1]
+
+        # Если противник захвачен в плен, вы выиграли
+        if location[i]==location[1-i]: return i
+    return -1
+
+print("!SIMPLE GAME " * 3)
+p1=makerandomtree(5)
+p2=makerandomtree(5)
+print(gridgame([p1,p2]))
+
+
+def tournament(pl):
+    # Массив для подсчета проигрышей
+    losses=[0 for p in pl]
+
+    # Каждый игрок встречается со всеми другими
+    for i in range(len(pl)):
+        for j in range(len(pl)):
+            if i==j: continue
+
+        # Кто выиграл?
+        winner=gridgame([pl[i],pl[j]])
+
+        # Два очка за поражение, одно за ничью
+        if winner==0:
+            losses[j]+=2
+        elif winner==1:
+            losses[i]+=2
+        elif winner==-1:
+            losses[i]+=1
+            losses[i]+=1
+            pass
+
+    # Отсортировать и вернуть результаты
+    z=zip(losses,pl)
+    z = sorted(z)#z.sort()
+    return z
+
+winner=evolve(5,100,tournament,maxgen=50)
+
+
+class humanplayer:
+    def evaluate(self,board):
+        # Получить мою позицию и позиции других игроков
+        me = tuple(board[0:2])
+        others = [tuple(board[x:x + 2]) for x in range(2, len(board) - 1, 2)]
+
+        # Нарисовать доску
+        for i in range(4):
+            for j in range(4):
+                if (i, j) == me:
+                    print('O')
+                elif (i, j) in others:
+                    print('X')
+                else:
+                    print('.')
+            print()
+
+        # Показать ходы, для справки
+        print('Ваш последний ход %d' % board[len(board)-1])
+        print(' 0')
+        print('2 3')
+        print(' 1')
+        print('Введите ход: ')
+
+        # Вернуть введенное пользователем число
+        move=int(input())
+        return move
+
+gridgame([winner,humanplayer( )])
